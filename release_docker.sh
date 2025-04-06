a06cfcb469ebd31161075215be57cc462cf24ff4
ricky <doraemonkey@qq.com>
2025-04-06 18:35:49 +0800

    fix: fix docker build

--- release_docker.sh
+++ release_docker.sh
@@ -4,6 +4,13 @@ COMMIT_HASH=$(git rev-parse HEAD)
 BUILD_TIME=$(date -Iseconds --utc)
 # BUILD_TAG=$(git describe --tags --abbrev=0 || echo 'unknown')
 BUILD_TAG=$(git tag --sort=-creatordate | head -n 1 || echo 'unknown')
+if [ "${BUILD_TAG}" = "" ]; then
+    BUILD_TAG="v0.0.0"
+fi
+
+echo "BUILD_TAG: ${BUILD_TAG}"
+echo "COMMIT_HASH: ${COMMIT_HASH}"
+echo "BUILD_TIME: ${BUILD_TIME}"
 
 docker build \
     --build-arg BUILD_HASH="${COMMIT_HASH}" \
