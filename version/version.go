7c2ce38b1ba67860ae24269a57507eddb758aba6
ricky <doraemonkey@qq.com>
2025-04-07 00:40:04 +0800

    release: v1.5.0

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 var (
-	Version   = "0.1.0"
+	Version   = "1.5.0"
 	BuildHash = "unknown hash" // will insert in build time
 	BuildTime = "unknown time" // will insert in build time
 )
