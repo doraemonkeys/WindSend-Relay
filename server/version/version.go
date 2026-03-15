e0602325afc858fa44d2c31b941e3f65bb6309b8
ricky <doraemonkey@qq.com>
2026-03-15 17:46:02 +0800

    release: v1.7.1

--- server/version/version.go
+++ server/version/version.go
@@ -3,7 +3,7 @@ package version
 import "fmt"
 
 var (
-	Version   = "1.6.0"
+	Version   = "1.7.1"
 	BuildHash = "unknown hash" // will insert in build time
 	BuildTime = "unknown time" // will insert in build time
 )
